777b9df44c9c3d62735b42862fd2343310d18d0e
adynah <t.barimalaa@gmail.com>
2025-06-28 04:10:40 +0100

    Added MySQLServer.py script to create alx_book_store database --Modified code to include 'except mysql.connector.Error'

--- MySQLServer.py
+++ MySQLServer.py
@@ -11,9 +11,12 @@ try:
     cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
 
-except Exception as e:
+except mysql.connector.Error as e:
     print("Error: Failed to connect to Database")
+    print("MySQL Error:", e)
 
 finally:
+    if 'cursor' in locals():
+        cursor.close()
     if 'mydb' in locals() and mydb.is_connected():
         mydb.close()
